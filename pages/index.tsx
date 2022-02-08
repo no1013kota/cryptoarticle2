c1b03fdccf0d449b03ecb35551f14e9cbd89c937
hinako <11hinako11@gmail.com>
2022-02-08 20:12:55 +0900

    add styled

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
+import { styled } from "@mui/material/styles";
 import {
   Button,
-  Card,
   AppBar,
   Toolbar,
   Typography,
   Grid,
-  CardContent,
-  CardActions,
   CardMedia,
+  Paper,
+  Card,
 } from "@mui/material";
 import type { InferGetStaticPropsType, NextPage } from "next";
 import Link from "next/link";
@@ -28,6 +28,23 @@ type Props = {
   blog: Blog[];
 };
 
+const BlogPaper = styled("div")({
+  padding: "16px",
+  transition: "all 0.2s",
+  "&:hover": {
+    boxShadow:
+      "1px 0px 5px -1px rgba(0,0,0,0.1), 0px 0px 5px 5px rgba(0,0,0,0.1), 0px 1px 5px 0px rgba(0,0,0,0.1)",
+    transform: "translateY(-1px)",
+  },
+});
+
+const HeadBar = styled(AppBar)({
+  backgroundColor: "#FFF",
+  color: "#222",
+});
+
+const image = "image/react.jpg";
+
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   blog,
 }: Props) => {
@@ -36,14 +53,15 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   const getDateStr = (date: string) => {
     return new Date(date).toLocaleDateString();
   };
+
   return (
     <>
       {/* ヘッダー */}
-      <AppBar position="static">
+      <HeadBar position="static">
         <Toolbar>
           <Typography>ヘッダー</Typography>
         </Toolbar>
-      </AppBar>
+      </HeadBar>
 
       {/* メインコンテナ */}
       <Grid container sx={{ p: 6 }}>
@@ -59,30 +77,32 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
           {blog.map((blog) => (
             <Grid item xs={12} sm={6} key={blog.id}>
               <Link href={`/blog/${blog.id}`} passHref>
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image="/static/images/cards/paella.jpg"
-                  alt="Paella dish"
-                />
-                <Card sx={{ p: 2, m: 2 }}>
-                  <Typography variant="h6" sx={{ pb: 1 }}>
-                    {blog.title}
-                  </Typography>
-                  {blog.tags.map((tag) => (
-                    <Typography
-                      key={tag.id}
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{ display: "inline-block", pr: 1 }}
-                    >
-                      #{tag.tag}
+                <a>
+                  <BlogPaper>
+                    <CardMedia
+                      component="img"
+                      height="200"
+                      image={image}
+                      alt="React"
+                    />
+                    <Typography variant="h6" sx={{ py: 1.5 }}>
+                      {blog.title}
                     </Typography>
-                  ))}
-                  <Typography key={blog.id} variant="body2">
-                    {getDateStr(blog.publishedAt)}
-                  </Typography>
-                </Card>
+                    {blog.tags.map((tag) => (
+                      <Typography
+                        key={tag.id}
+                        variant="body2"
+                        color="text.secondary"
+                        sx={{ display: "inline-block", pr: 1 }}
+                      >
+                        #{tag.tag}
+                      </Typography>
+                    ))}
+                    <Typography key={blog.id} variant="body2">
+                      {getDateStr(blog.publishedAt)}
+                    </Typography>
+                  </BlogPaper>
+                </a>
               </Link>
             </Grid>
           ))}
