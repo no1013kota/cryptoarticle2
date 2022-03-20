81c816d4d582f7f0491cc38084e8622b13aa6a1f
hinako <11hinako11@gmail.com>
2022-03-20 14:37:19 +0900

    update github actions

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,6 +1,8 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN || '',
-  apiKey: process.env.API_KEY || '',
+  serviceDomain: 'hinako',
+  apiKey: 'b4bd7f7358534c539c43862b89d298f6ca2f',
+  // serviceDomain: process.env.SERVICE_DOMAIN || '',
+  // apiKey: process.env.API_KEY || '',
 });
