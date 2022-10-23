8390b3361c710f3082be3aad575a5f98b182420c
hinakonagao <11hinako11@gmail.com>
2022-10-23 19:32:59 +0900

    Materital UIのエラー解消

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,35 @@
-import { memo } from "react";
+import { memo } from 'react';
 // materialUI
-import { Grid, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
+import { Grid, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-const FooterContainer = styled("div")({
-  backgroundColor: "#494949",
-  margin: "0 auto",
-  paddingTop: "30px",
-  paddingBottom: "30px",
-  color: "#f2f2f2",
-  letterSpacing: "0.08rem",
-  display: "flex",
-  justifyContent: "space-around",
-  flexWrap: "wrap",
+const FooterContainer = styled('div')({
+  backgroundColor: '#494949',
+  margin: '0 auto',
+  paddingTop: '30px',
+  paddingBottom: '30px',
+  color: '#f2f2f2',
+  letterSpacing: '0.08rem',
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexWrap: 'wrap',
 });
 
 export const Footer = memo(() => {
   return (
     <FooterContainer>
-      <Grid xs={10} md={8}>
-        <Grid item sx={{ textAlign: "center" }}>
-          <a href="https://twitter.com/napi_nami">
+      <Grid item xs={10} md={8}>
+        <Grid item sx={{ textAlign: 'center' }}>
+          <a href='https://twitter.com/napi_nami'>
             <TwitterIcon sx={{ m: 1 }} />
           </a>
-          <a href="https://github.com/hinakonagao">
+          <a href='https://github.com/hinakonagao'>
             <GitHubIcon sx={{ m: 1 }} />
           </a>
         </Grid>
-        <Typography
-          variant="body2"
-          sx={{ textAlign: "center", fontWeight: "200" }}
-        >
+        <Typography variant='body2' sx={{ textAlign: 'center', fontWeight: '200' }}>
           © 2022 hinako blog
         </Typography>
       </Grid>
