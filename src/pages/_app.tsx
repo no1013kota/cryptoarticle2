a1bc53c0f1af3eac15a1e172734695b50fe2bac6
no1013kota <matsubuz.10@gmail.com>
2023-01-15 18:20:29 +0900

    add-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../../Theme";
 import { RecoilRoot } from "recoil";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </RecoilRoot>
   );
